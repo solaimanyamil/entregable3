d8e666f2daa7983c0c0513f475f310681d6da804
solaimanyamil <139637947+solaimanyamil@users.noreply.github.com>
2023-08-24 16:27:10 -0300

    entregable3.py

--- entregable3.py
+++ entregable3.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from datetime import datetime, timedelta
-from airflow import DAG
+from airflow.models import DAG, Variable
 from airflow.providers.amazon.aws.operators.redshift import RedshiftSQLOperator
 from airflow.operators.python_operator import PythonOperator
 from funciones import load
+
+#               *************          VARIABLES              *************  
+
+db_username = Variable.get('db_username')
+db_password = Variable.get('db_password')
+db_name = Variable.get('db_name')
+db_host = Variable.get('db_host')
+db_port = Variable.get('db_port')
 
 #               ************* PARÁMETROS Y DEFINICIÓN DEL DAG *************
 
@@ -16,7 +24,7 @@ BC_dag = DAG(
     default_args=default_args,
     dag_id='entregable3',
     description='DAG para extracción, transformación y carga de datos',
-    start_date=datetime(2023,8,23),
+    start_date=datetime(2023,8,24),
     schedule_interval="@daily",
     catchup=False)
 
@@ -28,5 +36,6 @@ load = PythonOperator(
     python_callable=load,
     #op_args=["{{ ds }} {{ execution_date.hour }}"],
     dag=BC_dag)
+
 
 
